dc070719ce7d41f9c6906173ebe17693843396e4
diaaaaaa <ite.diaa@gmail.com>
2018-04-23 17:55:27 +0200

    initial web + security + thymeleaf

---
+++ src/main/java/at/refugeescode/mp19_pirates_of_the_codebean_5/Mp19PiratesOfTheCodeBean5Application.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp19_pirates_of_the_codebean_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp19PiratesOfTheCodeBean5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp19PiratesOfTheCodeBean5Application.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp19_pirates_of_the_codebean_5/Mp19PiratesOfTheCodeBean5ApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp19_pirates_of_the_codebean_5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp19PiratesOfTheCodeBean5ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
